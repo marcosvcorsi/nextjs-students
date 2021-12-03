b201b6a165004e6c5614106d4931a4860c1265b8
Marcos Corsi <marcosvcorsi@gmail.com>
2021-12-03 17:03:22 -0300

    feat: add course behavior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,211 @@
-import gql from 'graphql-tag'
 import type { NextPage } from 'next'
-import Link from 'next/link';
+import {
+  Heading,
+  Container,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+  FormLabel,
+  IconButton,
+  Icon,
+  useToast
+} from '@chakra-ui/react';
+import { FaUserGraduate } from 'react-icons/fa'
+import { useDisclosure } from '@chakra-ui/hooks'
+import { AddIcon, EditIcon } from '@chakra-ui/icons'
+import gql from 'graphql-tag'
+import Router from 'next/router';
 import { client } from './services/graphql'
+import { FormEvent, useState } from 'react';
 
-type HomeProps = {
-  courses: {
-    id: string;
-    name: string;
-  }[]
+type Course = {
+  id: string;
+  name: string;
 }
 
-const Home: NextPage<HomeProps> = ({ courses }) => {
-  return (
-    <>
-      <header>
-        <h1>University</h1>
-      </header>
-      <main>
-        <h3>Courses</h3>
+type HomeProps = {
+  courses: Course[]
+}
 
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {courses.map(course => (
-              <tr key={course.id}>
-                <td>{course.name}</td>
-                <td>
-                  <Link href={`/courses/${course.id}`}>
-                    <a>Students</a>
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </main>
-    </>
+const Home: NextPage<HomeProps> = ({ courses: courseInitialList }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
+
+  const [course, setCourse] = useState<Partial<Course> | null>(null)
+  const [courses, setCourses] = useState<Course[]>(courseInitialList || []);
+
+  const openCourseModal = (course?: Course) => {
+    setCourse(course ? course : { name: '' });
+
+    onOpen();
+  }
+
+  const handleSaveCourse = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      if(!course) {
+        return;
+      }
+
+      const { data } = await saveCourse(course);
+
+      toast({
+        title: 'Success',
+        description: 'Course was save successfully',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+
+      const { course: savedCourse } = data;
+
+      const newCourses = [...courses];
+
+      const existingCourseIndex = newCourses
+        .findIndex(course => course.id === savedCourse.id);
+
+      if(existingCourseIndex !== -1) {
+        newCourses[existingCourseIndex] = savedCourse;
+      } else {
+        newCourses.push(savedCourse);
+      }
+
+      setCourses(newCourses)
+      setCourse(null);
+      onClose();
+    } catch(error: any) {
+      console.error(JSON.stringify(error));
+
+      toast({
+        title: 'Ops something is wrong',
+        description: error.message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+  }
+
+  const saveCourse = async (course: Partial<Course>) => {
+    const variables = {
+      data: {
+        name: course.name,
+      }
+    };
+
+    if(course.id) {
+      Object.assign(variables.data, { id: course.id })
+
+      return client.mutate({
+        mutation: gql`
+          mutation updateCourse($data: UpdateCourseInput!) {
+            course: updateCourse(data: $data) {
+              id
+              name
+            }
+          }
+        `,
+        variables,
+      })
+    }
+
+    return client.mutate({
+      mutation: gql`
+        mutation createCourse($data: CreateCourseInput!) {
+          course: createCourse(data: $data) {
+            id
+            name
+          }
+        }
+      `,
+      variables,
+    }) 
+  }
+
+  return (
+    <Container>
+      <Heading as="h3">Courses</Heading>
+
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Actions</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {courses.map(course => (
+            <Tr key={course.id}>
+              <Td>{course.name}</Td>
+              <Td>
+                <IconButton
+                  aria-label="Students" 
+                  icon={<Icon as={FaUserGraduate} />}
+                  onClick={() => Router.push(`/courses/${course.id}`)}
+                  mr={1}
+                />
+
+                <IconButton
+                  aria-label="Edit Course" 
+                  icon={<EditIcon />}
+                  onClick={() => openCourseModal(course)}
+                />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+
+      <Button 
+        leftIcon={<AddIcon />} 
+        colorScheme='teal' 
+        onClick={() => openCourseModal()}
+        mt={3}
+      >
+        Create a course
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Course</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>
+            <form id="form-course" onSubmit={handleSaveCourse}>
+              <FormLabel htmlFor='name'>Name</FormLabel>
+              <Input
+                id='name'
+                placeholder='Please enter a name'
+                value={course?.name || ''}
+                onChange={(event) => setCourse({ ...course, name: event.target.value })}
+              />
+            </form>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} type="submit" form="form-course">Save</Button>
+
+            <Button variant='ghost' onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Container>
   )
 }
 
